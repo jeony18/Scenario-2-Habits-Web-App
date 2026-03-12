03c2a8be691df9200b08cdc2e83f717b507e4837
domlee2004 <domelee2004@gmail.com>
2026-03-12 10:55:35 +0000

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import { useState } from "react";
+import HomePage from "./components/HomePage";
+import RecommendationsPage from "./components/RecommendationsPage";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const [userPreferences, setUserPreferences] = useState(null);
+
+  const handleGetActivities = (preferences) => {
+    setUserPreferences(preferences);
+    setCurrentPage("recommendations");
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +23,19 @@ function App() {
       </Routes>
     </BrowserRouter>
   )
+    <div>
+      {currentPage === "home" ? (
+        <HomePage onGetActivities={handleGetActivities} />
+      ) : (
+        <RecommendationsPage
+          userPreferences={userPreferences}
+          onBack={() => setCurrentPage("home")}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App
+
+export default App;
